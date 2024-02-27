fe43d06af89e628e0d46e39b2471d0ad26e40835
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2024-02-27 00:12:27 -0300

    Added comments to backend rebuild script

--- backend/backend-rebuild.sh
+++ backend/backend-rebuild.sh
@@ -1,5 +1,14 @@
+# Stop container
 docker container stop godeck-spring-boot
+
+# Remove container
 docker container rm godeck-spring-boot
+
+# Remove image
 docker image rm backend_spring-boot
+
+# Recompile application
 mvn clean install
+
+# Build image and run container
 docker-compose up -d --build

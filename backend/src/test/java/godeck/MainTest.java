abaa1a72f2c63fb9801ac557cd84605a59f54c17
Bruno Pena Baeta <brunopenabaeta@outlook.com>
2024-03-07 17:01:24 -0300

    Created test files

---
+++ backend/src/test/java/godeck/MainTest.java
@@ -0,0 +1,10 @@
+package godeck;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void testMain() {
+
+    }
+}
---
+++ backend/src/test/java/godeck/game/GameClientTest.java
@@ -0,0 +1,15 @@
+package godeck.game;
+
+import org.junit.jupiter.api.Test;
+
+public class GameClientTest {
+    @Test
+    void testRun() {
+
+    }
+
+    @Test
+    void testSetupGameClient() {
+
+    }
+}
